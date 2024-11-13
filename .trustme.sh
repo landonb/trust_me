c9396e338770b99d9ce7eec87ef8d913593a5b8f
Landon Bouma <landonb@users.noreply.github.com>
2024-11-13 14:42:54 -0600

    Improve: Convert to logger.sh dependency

--- .trustme.sh
+++ .trustme.sh
@@ -69,9 +69,8 @@ source_plugin () {
   TRUSTME_DIR="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
   TRUSTME_PRE="${TRUSTME_BASENAME:-.trustme}"
 
-  . "${TRUSTME_DIR}/deps/sh-colors/bin/colors.sh"
-
-  source_home_fries_util 'logger.sh'
+  # Load logger.sh, which also loads colors.sh
+  . "${TRUSTME_DIR}/deps/sh-logger/bin/logger.sh"
 
   PROJ_PLUGIN="${TRUSTME_DIR}/${TRUSTME_PRE}.plugin"
   if [ ! -f "${PROJ_PLUGIN}" ]; then
@@ -81,49 +80,6 @@ source_plugin () {
   fi
 
   source "${PROJ_PLUGIN}"
-}
-
-source_home_fries_util () {
-  local home_fries_util="$1"
-  local source_path="${home_fries_util}"
-  local log_success=false
-
-  # DEV: Uncomment to get help with source errors.
-  #  log_success=true
-  # If the /home/user/.kit/sh/home-fries/lib path is on $PATH, you can just source it.
-  if ! . "${source_path}" &> /dev/null; then
-    # But if it's not on $PATH, see if this script is a symlink, and if so,
-    # see if the util file is part of this file's owning repo.
-    if [ -h "${BASH_SOURCE[0]}" ]; then
-      # If this script is symlinked, checked its real path for the source file.
-      source_path="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")/${home_fries_util}"
-      if ! . "${source_path}" &> /dev/null; then
-        # 2018-05-16 11:30: Ug, this fcn. is a mess now! So nested!
-        # Use commonly used Home Fries location if not found so far.
-        # What's up? If you run Vim from terminal, it inherits your
-        # $PATH (and home-fries/lib is on your PATH, right?); but if
-        # you run Vim from Gnome Launcher, or from Keyboard Shortcut,
-        # it's got a basic $PATH. Real solution is to fix PATH from
-        # your .vimrc; but we can be nice and patch it here, too.
-        source_path="${SHOILERPLATE:-${DOPP_KIT:-${HOME}/.kit}/sh}/reputed-tiler/bin/reputed-tiler/lib/${home_fries_util}"
-        if ! . "${source_path}" &> /dev/null; then
-          >&2 echo "Unable to find and source ${home_fries_util}. You're missing out!"
-          # Rather than not exiting now, we could keep running, if we chose
-          # to not use the libraries being loaded (2018-05-16: which is
-          # just the logger and the color library), we could write our code
-          # to work without said libraries. But it's not wired that way.
-
-          exit 1
-        elif $log_success; then
-          echo "Sourced: Found in home-fries: ${source_path}"
-        fi
-      elif $log_success; then
-        echo "Sourced: Inferred path from symlink: ${source_path}"
-      fi
-    fi
-  elif $log_success; then
-    echo "Sourced: Already on \$PATH: ${source_path}"
-  fi
 }
 
 # ***
